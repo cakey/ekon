607728e870c7dcce520e1a1058d4334f93b8b37d
Benjamin Shaw <benjamin.shaw@salesforce.com>
2014-06-17 23:28:26 -0700

    created basic world

--- sim.py
+++ sim.py
@@ -1,8 +1,57 @@
+import random
+
 import agents
 
-def run_sim(rounds=100):
+
+def build_graph(node_count):
+
+    graph = {}
+
+    for node_num in range(node_count):
+        graph[node_num] = {n:1 for n in range(node_count) if n != node_num} 
+
+    return graph
+
+
+def run_sim():
+    num_rounds = 100
+    traveller_start_gold = 1000
+    resource_price = [5,15]
+    resource_count = 5
+    node_count = 10
+
+    world_graph = build_graph(node_count)
+    world_shops = {
+        name: {
+            "GOLD": {
+                "buy": random.randint(10,100),
+                "sell": random.randint(10,100),
+                "quantity": random.randint(10,100)
+            },
+            "SILVER": {
+                "buy": random.randint(10,100),
+                "sell": random.randint(10,100),
+                "quantity": random.randint(10,100)
+            },
+            "NANOCHIPS": {
+                "buy": random.randint(10,100),
+                "sell": random.randint(10,100),
+                "quantity": random.randint(10,100)
+            }
+        } for name in world_graph.keys()}
+
+    world_agents = [{
+            "name":name,
+            "func":func,
+            "coin": 1000,
+            "location": random.choice(world_graph.keys())
+        } for name,func in agents.agents.iteritems()]
+
+    print world_graph
+    print world_shops
+    print world_agents
+
     print "everyone loses"
-    print agents.agents
 
 if __name__ == '__main__':
     run_sim()
