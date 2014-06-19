0bbe6ad3298c6f90fda3166cf1710f6204a4c73b
Benjamin Shaw <benjamin.shaw@salesforce.com>
2014-06-18 22:33:10 -0700

    uncommented matts agent

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,6 +2,6 @@ import matt_shitty_agent
 import east_india_trade_company
 
 agents = {
-  #"matt_shitty_agent": matt_shitty_agent.agent,
+  "matt_shitty_agent": matt_shitty_agent.agent,
   "stewart_awesome_agent": east_india_trade_company.agent
 }
