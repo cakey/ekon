ffce8ce23ec724c66f1c49b2f90e3f1363080c82
Chris Howard <chrishoward91@gmail.com>
2014-06-20 00:45:12 -0700

    Trailing whitespace ya cunt

--- agents/utils.py
+++ agents/utils.py
@@ -1,9 +1,9 @@
 def is_last_round(world_state):
     """ Returns whether it is the last round"""
-    
+
     current_round = world_state["meta"]["current_round"]
     round_count = world_state["meta"]["total_rounds"]
-    
+
     return current_round == round_count-1
 
 def neighbours(world_state, label=None):
