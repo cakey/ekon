546293a419b530e915c4dcdfc68f43ac77385ea5
Benjamin Shaw <benjamin.shaw@salesforce.com>
2014-06-19 21:54:49 -0700

    i am disappoint stew

--- agents/utils.py
+++ agents/utils.py
@@ -1,9 +1,7 @@
-#Determines if it is the last round
 def is_last_round(world_state):
+    """ Returns whether it is the last round"""
     
     current_round = world_state["meta"]["current_round"]
     round_count = world_state["meta"]["total_rounds"]
     
-    if current_round == round_count-1:
-    	return True
-    return False
+    return current_round == round_count-1
